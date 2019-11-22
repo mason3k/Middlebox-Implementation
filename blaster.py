47be58c708af980393acfcd4653aa0add88663e3
mason3k <sydney.u.jones@gmail.com>
2019-11-21 20:56:36 -0600

    First Attempt - Incomplete

--- blaster.py
+++ blaster.py
@@ -12,6 +12,15 @@ def switchy_main(net):
     mymacs = [intf.ethaddr for intf in my_intf]
     myips = [intf.ipaddr for intf in my_intf]
 
+    sliding_window = SlidingWindow()
+
+    blastee_IP = get_file_info("b")
+    num_packets = get_file_info("n")
+    payload_len = get_file_info("l")
+    sender_window_size = get_file_info("w")
+    RTT = get_file_info("rtt")
+    recv_timeout = get_file_info("alpha")
+
     while True:
         gotpkt = True
         try:
@@ -26,6 +35,9 @@ def switchy_main(net):
 
         if gotpkt:
             log_debug("I got a packet")
+            #TODO if we got an ack:
+            #TODO if the ack seq number is in the window (use is_seqNo_in_window):
+            #   -> Remove the entry and refresh LHS (use refresh_LHS)
         else:
             log_debug("Didn't receive anything")
 
@@ -40,3 +52,47 @@ def switchy_main(net):
             '''
 
     net.shutdown()
+
+def get_file_info(key):
+    f = open("blaster_params.txt", "r")
+    key = "-"+key
+    for line in f:
+        line_list = line.split(" ")
+        key_index = line_list.index(key)
+        if key_index < line_list.len():
+            return line_list[key_index + 1]
+        else:
+            return ""
+
+
+class SlidingWindow(object,LHS = 1,RHS = 1):
+
+    def __init__(self):
+        self.window = []
+        self.LHS = LHS
+        self.RHS = RHS
+
+    def add_entry(self,entry):
+        self.window.append(entry)
+
+    def is_seqNo_in_window(self,seqNo):
+        pos = seqNo - self.LHS
+        mas_pos = max - min + 1
+        return pos < mas_pos
+
+    def refresh_LHS(self,entry):
+        for entry in window:
+            if entry.is_acked == False:
+                return
+            else:
+                self.window.remove(entry)
+                self.LHS +=1
+        return
+
+
+class SlidingWindowEntry(object):
+    
+    def __init__(self, seq_no = None, time_last_sent = time.time()):
+        self.is_acked = False
+        self.seq_no = seq_no
+        self.time_last_sent = time_last_sent
